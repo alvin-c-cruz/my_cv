6fb83efc907daad8697660835af31a203275f842
alvin-c-cruz <alvinccruz12@gmail.com>
2022-03-26 11:46:33 +0700

    updated /hello

--- my_cv/blueprints/hello_world/routes.py
+++ my_cv/blueprints/hello_world/routes.py
@@ -1,26 +1,28 @@
 from flask import Blueprint, render_template, request, current_app
 import subprocess
-import os
+import sys
 
 bp = Blueprint('hello_world', __name__, template_folder='pages', url_prefix='/hello')
 
 
 @bp.route('/', methods=['POST', 'GET'])
 def home():
+    code = ""
+    output = ""
+
     if request.method == 'POST':
         code = request.form['code']
 
-        file_path = os.path.join(current_app.instance_path, 'temp', 'hello.py')
-        with open(file_path, 'w+') as file:
-            file.write(code)
+        if 'import' in code:
+            pass
+        else:
+            process_output = subprocess.run(
+                [sys.executable, "-c", code], capture_output=True, text=True
+                )
 
-        command = f'python {file_path}'
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        output, error = process.communicate()
-        output = output.decode().replace('b"', '')
-        print(output, type(output))
-    else:
-        code = ""
-        output = ""
-        error = ""
-    return render_template('hello_world/home.html', code=code, output=output, error=error)
+            if process_output.stderr:
+                output = process_output.stderr.replace('File "<string>", ', '')
+            else:
+                output = process_output.stdout
+
+    return render_template('hello_world/home.html', code=code, output=output)
